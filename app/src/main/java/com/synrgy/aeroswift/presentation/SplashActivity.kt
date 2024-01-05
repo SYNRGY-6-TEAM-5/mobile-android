78d6fdc32206061f35d1b6e89a84ed70910660e8
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2024-01-05 21:59:51 +0700

    fix: fix oauth login

--- app/src/main/java/com/synrgy/aeroswift/presentation/SplashActivity.kt
+++ app/src/main/java/com/synrgy/aeroswift/presentation/SplashActivity.kt
@@ -3,12 +3,17 @@ package com.synrgy.aeroswift.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.activity.viewModels
 import com.synrgy.aeroswift.R
 import com.synrgy.aeroswift.databinding.ActivityMainBinding
 import com.synrgy.aeroswift.databinding.ActivitySplashBinding
+import com.synrgy.aeroswift.presentation.viewmodel.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,21 @@ class SplashActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        authViewModel.checkAuth()
+
+        authViewModel.authentication.observe(this, ::handleNavigateToActivity)
+    }
+
+    private fun handleNavigateToActivity(token: String) {
         Handler().postDelayed({
-            MainActivity.startActivity(this)
+            if (token.isNotEmpty() && token.isNotBlank()) {
+                authViewModel.setToken(token)
+                HomeActivity.startActivity(this)
+                this.finish()
+            } else {
+                MainActivity.startActivity(this)
+                this.finish()
+            }
         }, 2000)
     }
 }
