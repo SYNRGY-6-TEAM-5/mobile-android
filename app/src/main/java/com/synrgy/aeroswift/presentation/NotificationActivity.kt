f45e5c51a846bbd3d8bad5bb5234a042667e14ef
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2024-01-28 14:53:47 +0700

    feat: add logout dialog

---
+++ app/src/main/java/com/synrgy/aeroswift/presentation/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.synrgy.aeroswift.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.synrgy.aeroswift.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
